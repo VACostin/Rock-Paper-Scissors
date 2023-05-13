987640021783342181defb0ac63ffa57adbb0af6
VACostin <virlan.costin.andrei@gmail.com>
2023-05-14 02:41:37 +0300

    Coded rps logic

--- javascript.js
+++ javascript.js
@@ -4,44 +4,41 @@ function getComputerChoice() {
   return ROCK_PAPER_SCIS[computer_roll];
 }
 
-function gameON(playerSelection, computerSelection) {
-  const ROCK_PAPER_SCIS = ["ROCK", "PAPER", "SCISSORS"];
-  playerSelection = playerSelection.toUpperCase();
-  if (ROCK_PAPER_SCIS.includes(playerSelection)) {
-    //I haven't done this for fancy reasons, I just want run through a switch and the ? : thing"
-    let result = playerSelection.localeCompare(computerSelection);
-    switch (result) {
-      case 0:
-        return "tie";
-      case 1:
-        return (playerSelection + computerSelection == "SCISSORSPAPER" ? "win" : "loss");
-      case -1:
-        return (playerSelection + computerSelection == "PAPERSCISSORS" ? "loss" : "win");
-    }
+function playRound(playerSelection) {
+  //I haven't done this for fancy reasons, I just want run through a switch and the ? : thing"
+  let computerSelection = getComputerChoice();
+  let result = playerSelection.localeCompare(computerSelection);
+  switch (result) {
+    case 0:
+      return "tie";
+    case 1:
+      return (playerSelection + computerSelection == "SCISSORSPAPER" ? "win" : "loss");
+    case -1:
+      return (playerSelection + computerSelection == "PAPERSCISSORS" ? "loss" : "win");
   }
 }
 
-let ongoing = true;
+const buttons = document.querySelectorAll('button');
+
 let playerScore = 0;
 let computerScore = 0;
-while (ongoing) {
-  const computerChoice = getComputerChoice();
-  const playerChoice = prompt();
-  switch (gameON(playerChoice, computerChoice)) {
-    case "win": playerScore++;
-      console.log("Computer chose " + computerChoice + ". Player wins this round!");
-      break;
-    case "loss": computerScore++;
-      console.log("Computer chose " + computerChoice + ". Computer wins this round!");
-      break;
-    default: break;
-  }
-  if (playerScore == 5) {
-    console.log("Humaniy wins");
-    ongoing = false;
-  }
-  else if (computerScore == 5) {
-    console.log("AI wins");
-    ongoing = false;
-  }
-}
+const playerDisplay = document.getElementById("PlayerScore");
+const computerDisplay = document.getElementById("MachineScore");
+
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    let playerChoice = button.innerText;
+    let result = playRound(playerChoice);
+    switch (result) {
+      case "win": playerScore++;
+      playerDisplay.textContent = playerScore;
+        break;
+      case "loss": computerScore++;
+      computerDisplay.textContent = computerScore;
+        break;
+      default: break;
+    }
+  });
+});
