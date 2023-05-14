c83ab8e0c97e6e3439817c19c7db1428776f62e5
VACostin <virlan.costin.andrei@gmail.com>
2023-05-14 16:50:31 +0300

    Coded (in a rush) display logic + score

--- javascript.js
+++ javascript.js
@@ -8,6 +8,8 @@ function playRound(playerSelection) {
   //I haven't done this for fancy reasons, I just want run through a switch and the ? : thing"
   let computerSelection = getComputerChoice();
   let result = playerSelection.localeCompare(computerSelection);
+  let resultText = "Computer played: " + computerSelection + ". Player played: " + playerSelection;
+  computerVSplayer.textContent = resultText;
   switch (result) {
     case 0:
       return "tie";
@@ -24,6 +26,8 @@ let playerScore = 0;
 let computerScore = 0;
 const playerDisplay = document.getElementById("PlayerScore");
 const computerDisplay = document.getElementById("MachineScore");
+const whoWins = document.getElementById("WhoWins");
+const computerVSplayer = document.getElementById("Result");
 
 buttons.forEach((button) => {
 
@@ -33,12 +37,33 @@ buttons.forEach((button) => {
     let result = playRound(playerChoice);
     switch (result) {
       case "win": playerScore++;
-      playerDisplay.textContent = playerScore;
+        playerDisplay.textContent = playerScore;
+        whoWins.textContent = "Player Wins";
+        if (playerScore >= 5) {
+          alert("Player wins");
+          playerScore = 0;
+          computerScore = 0;
+          playerDisplay.textContent = playerScore;
+          computerDisplay.textContent = computerScore;
+          whoWins.textContent = "";
+          computerVSplayer.textContent = "";
+        }
         break;
       case "loss": computerScore++;
-      computerDisplay.textContent = computerScore;
+        computerDisplay.textContent = computerScore;
+        whoWins.textContent = "Computer Wins";
+        if (computerScore >= 5) {
+          alert("Computer wins");
+          playerScore = 0;
+          computerScore = 0;
+          playerDisplay.textContent = playerScore;
+          computerDisplay.textContent = computerScore;
+          whoWins.textContent = "";
+          computerVSplayer.textContent = "";
+        }
         break;
-      default: break;
+      default: whoWins.textContent = "It's a tie";
+        break;
     }
   });
 });
